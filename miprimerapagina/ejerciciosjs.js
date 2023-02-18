b0caf037a4458b94ceffba5253f0cbc9ba134593
Ezequiel <ezeabregu.ea@gmail.com>
2023-02-18 02:00:40 -0300

    add .

--- miprimerapagina/ejerciciosjs.js
+++ miprimerapagina/ejerciciosjs.js
@@ -1,139 +1,101 @@
-// Nuestra primera linea en JS
-// alert('Hola Mundo Nucba');
 
-// Variables
-// Para declarar una variable usamos la palabra reservada de var / let / const, le damos un nombre y lo igualamos a un valor
-var numeroA = 15;
-var numeroB = 20;
-var resultado = numeroA + numeroB;
+/**************CONDICIONALES************************************/
 
-// Para mostrar en la consola vamos a hacer uso del console.log()
-// console.log(resultado);
+// let nombre = 'Silvana';
+// let edad = 29;
+// let numeroA = 32;
+// let numberoB = 29;
 
-// Vamos a concatenar texto
-var textoA = 'Harry';
-var textoB = 'Potter';
+//console.log(nombre,edad);
 
-// consoleamos textoA + textoB y le agregamos un espacio
-// console.log(textoA + ' ' + textoB);
+//console.log(`Hola! Mi nombre es ${nombre} y tengo ${edad} años`);
 
-// consoleamos textoA + numeroA y nos devuelve Harry15
-// console.log(textoA + numeroA);
+//console.log(numeroA + numberoB);
 
-// * Tipos de datos en Js
-/*
-- Number = 1/35/155/49...
-- String = 'Hola', "Hola", `Hola`
-- Booleanos = true, false
-- undefined = algo no definido
-- null = vacio
-- funciones
-- objetos
-- arrays
-*/
+    // let numero = 15;
+    // numero = 25;
 
-// Ejemplo con template strings ``
-// Vamos a usar las variables de textoA y textoB
-// console.log('La pelicula es: ' + textoA + ' ' + textoB);
-// console.log(`La pelicula es: ${textoA} ${textoB}`);
+    // console.log(numero);
 
-// Podemos reasignar valores a las variables
-var nombre = 'Nelson';
-// console.log(nombre);
-nombre = 'Pepito';
-// console.log(nombre);
+// let mensaje = "Cuidado!"
 
-// Var let const
+// alert(mensaje);
 
-// Var lo podemos volver a declarar con el mismo nombre y asignar un nuevo valor
-var tipoVar = 'Hola soy var';
-// console.log(tipoVar);
-var tipoVar = 'hola soy un var redeclarado';
-// console.log(tipoVar);
+// let valor = prompt("Ingrese su edad:")
+// console.log(valor);
 
-// Let
-let tipoLet = 'Hola soy let';
-//* No puedo volver a declararla con el mismo nombre
-// let tipoLet = 'Hola soy let redeclarado no puedo';
+// let nombre = prompt("Ingrese su nombre:");
+// alert(`El nombre ingresado es ${nombre}`);
 
-// * Lo que podemos hacer es reasignar su valor
-tipoLet = 'Hola soy let reasignado';
-// console.log(tipoLet);
+// let nombre1 = prompt("Ingrese primer nombre:");
+// let nombre2 = prompt("Ingrese segundo nombre:");
+// alert(`El primer nombre es ${nombre1} y el segundo ${nombre2}`);
 
-// !Ejemplo
-const nombreConst = 'Nelson';
-let edad = 27;
+// let edad = prompt("Ingrese su edad:");
+// if(edad >= 18)
+// {
+//     console.log("Eres mayor de edad.");
+// }
+// else
+// {
+//     console.log("Eres menor de edad.");
+// }
 
-// Const
-const tipoConst = 'Hola soy const';
-// * No podemos volver a declararla con el mismo nombre
-// const tipoConst = 'Hola soy const redeclarado, no puedo'
-// * No podemos reasignar su valor
-// tipoConst = 'Hola, no podemos reasiganar su valor';
-// console.log(tipoConst);
+// let valor1 = 65;
+// let valor2 = 5;
+// if (valor1 > valor2) 
+// {
+//     console.log("El valor 1 es mayor que el valor 2.");
+// } else if (valor2 > valor1)
+// {
+//     console.log("El valor 2 es mayor que el valor 1.");
+// }
+// else
+// {
+//     console.log("Los valores son iguales.");
+// }    
 
-// * Operadores
-/*
-+ = sumar
-- = resta
-* = multiplicacion
-/ = division
-** = calcular la base a la potencia del exponente
-% = modulo, nos da el resto
-++ = incrementar, suma 1
--- = decrementador, restar 1
-*/
+// let palabra = prompt("Ingrese valor:");
+// if(palabra === "alerta" ){
+//     alert("Soy una alerta");
+// }else if(palabra === "consola"){
+//     console.log("Soy un mensaje en la consola");
+// }else{
+//     alert("Me muestro en ambos lados");
+//     console.log("Me muestro en ambos lados");
+// }
 
-let numeritoUno = 10;
-let numeritoDos = 5;
+// let edad = 98;
 
-// suma
-// console.log(numeritoUno + numeritoDos);
-// restar
-// console.log(numeritoUno - numeritoDos);
-// multiplicar
-// console.log(numeritoUno * numeritoDos);
-// Division
-// console.log(numeritoUno / numeritoDos);
-// modulo
-// console.log(numeritoUno % numeritoDos);
-// incrementar, le incrementamos 1 y despues lo consoleamos
-// numeritoUno++;
-// console.log(numeritoUno);
-// decrementar, le decrementamos 1 y despues consoleamos
-// numeritoDos--;
-// console.log(numeritoDos);
+// if (edad < 20 && edad < 13) {
+//     console.log("Soy un niño");
+// } else if(edad < 20 && edad >= 13){
+//     console.log("Soy un adolescente");
+// } else if(edad >= 20 && edad < 50){
+//     console.log("Soy un adulto");
+// } else {
+//     console.log("Soy un adulto mayor");
+// }
 
-// * Operadores de comparacion
-/*
-== => compara valores
-=== => compara los valores y el tipo de dato
-!= => diferentes valores
-!== => difentes valores y diferente tipo de dato
-> => mayor que
-< => menor que
->= => mayor o igual
-<= => menor o igual
-&& = y - and
-|| = o - or
-*/
+// let fruta = 'durazno';
 
-// console.log(5 == 5); devuelve true
-// console.log(5 == '5'); devuelve true
-// console.log(5 === '5'); devuelve false
+// switch (fruta) {
+//     case 'manzana':
+//         console.log("Soy una manzana");
+//         break;
+//     case 'frutilla':
+//         console.log("Soy una frutilla");
+//         break;
+//     case 'durazno':
+//         console.log("Soy un durazno");
+//         break;
+//     case 'banana':
+//         console.log("Soy una banana");
+//         break;
+//     default:
+//         console.log("No se que soy");
+//         break;
+// }
 
-// Como sabemos que tipo de dato estamos manejando?
-// Podemos usar typeof
-// console.log(typeof 5);
-// console.log(typeof '5');
-// console.log(typeof true);
-// console.log(typeof 5.3);
+/**************CICLOS***************************************/
 
-// Prompt
-// prompt('Hola, como te llamas?') // Abre el cuadro de dialogo que nos pide un dato
-// Nosotros queremos guardar ese dato para consolearlo, entonces lo guardamos en una variable
-// let dato = prompt('Hola, como te llamas?');
-// Al guardar en una variable dato va a valor lo que le escribamos en el prompt
-// consoleamos el dato
-// console.log(dato);
-// clg Atajo para el console.log()
