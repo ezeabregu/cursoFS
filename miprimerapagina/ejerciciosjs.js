1dda3cde310d6adee7b12872ffb7a46b29c0134d
Ezequiel <ezeabregu.ea@gmail.com>
2023-03-15 23:17:42 -0300

    clase js

--- miprimerapagina/ejerciciosjs.js
+++ miprimerapagina/ejerciciosjs.js
@@ -234,4 +234,440 @@ viceversa.*/
 
 // arreglarTexto();
 
-/**************FUNCIONES y ARRAYS*******************************/
+/**************ARRAY DE OBJETOS*******************************/
+
+/*1)
+Utilizando forEach, crea una función que reciba un array de 
+palabras y devuelva otro solamente con las palabras que incluyan
+ la letra “m” (tanto mayúscula como minúscula).*/
+
+// const palabras = ['Mandibula','Craneo','Medula osea',
+// 'Cadera mecanica','Oido',];
+
+// const palabrasConM = (palabras) => {
+//   let nuevoArray = [];
+//   palabras.forEach((palabra) => {
+//     if (palabra.toLowerCase().includes("m")) {
+//       nuevoArray.push(palabra);
+//     }
+//   });
+//   console.log(nuevoArray);
+//   return nuevoArray;
+// };
+
+// palabrasConM(palabras);
+
+/*2)
+Utilizando forEach, crea una función que reciba dos números y un 
+array de números y que devuelva otro array con aquellos números
+ que estén en el array y sean mayores al primer numero dado y 
+ menores al segundo ( sin incluirlos).*/
+
+// const arrayNumeros=[2,3,53,6,3,23,665,3,34,2];
+
+// const numerosEnIntervalo = (num1, num2, numeros) => {
+//   let nuevoArray = [];
+//   numeros.forEach((numero) => {
+//     if (numero > num1 && numero < num2) {
+//       nuevoArray.push(numero);
+//     }
+//   });
+//   console.log(nuevoArray);
+//   return nuevoArray;
+// };
+
+// numerosEnIntervalo(8, 300, arrayNumeros);
+
+/*3)
+Utilizando forEach, crea una función que reciba dos números y un 
+array de números y que devuelva el mismo array pero con los
+ números mayores al primer numero dado multiplicados por el 
+ segundo numero dado. En caso de que no sean mayores al número 
+ dado, devolver el mismo número en esa posición.
+
+Ejemplo: Si tenemos un array de números del 1 al 5 , y llamamos a
+ multiplicarMayoresA(3,10,numeros) , el output será : 
+ [1,2,3,40,50]*/
+
+// const arrayNumeros = [1, 2, 6, 8, 11, 12, 13, 25, 26];
+
+// const multiplicarMayoresA = (limite, multiplicador, numeros) => {
+//   let nuevoArray = [];
+//   numeros.forEach((numero) => {
+//     if (numero > limite) {
+//       nuevoArray.push(numero * multiplicador);
+//     } else {
+//       nuevoArray.push(numero);
+//     }
+//   });
+//   console.log(nuevoArray);
+//   return nuevoArray;
+// };
+
+// multiplicarMayoresA(4, 10, arrayNumeros);
+
+/*4)
+Crea una función que reciba un array de números y un número que 
+EN CASO DE QUE EXISTA al menos un número mayor al dado, retorne 
+el primero que cumpla con esa condición. En caso contrario, que
+retorne “No hay números mayores al número dado”*/
+
+// const arrayNumeros = [1, 2, 3, 4, 5];
+
+// const buscarNumero = (numeros, numeroLimite) =>
+//   numeros.some((numero) => numero > numeroLimite)
+//     ? numeros.find((numero) => numero > numeroLimite)
+//     : "No hay numeros mayores al número dado";
+
+// // Es lo mismo que escribir
+
+// const buscarNumeroAlt = (numeros, numeroLimite) => {
+//   if (numeros.some((numero) => numero > numeroLimite)) {
+//     return numeros.find((numero) => numero > numeroLimite);
+//   } else {
+//     return "No hay numeros mayores al número dado";
+//   }
+// };
+
+// console.log(buscarNumero(arrayNumeros, 2));
+
+/*5)
+Crea una función que reciba un array de palabras que devuelva 
+la primer palabra del array ( la primera que se encuentre) que
+tenga mas de 4 letras. En caso de que no haya, deberá 
+un string que diga : “No hay palabras con mas de 4 letras”*/
+
+// const arrayPalabras = ["hola", "bienvenidos", "a", "la", "Nucbanetta"];
+
+// const buscarPalabras = (palabras) =>
+//   palabras.some((palabra) => palabra.length > 4)
+//     ? palabras.find((palabra) => palabra.length > 4)
+//     : "No hay palabras con mas de 4 letras";
+
+// // Esto es lo mismo que escribir:
+
+// const buscarPalabrasAlt = (palabras) => {
+//   if (palabras.some((palabra) => palabra.length > 4)) {
+//     return palabras.find((palabra) => palabra.length > 4);
+//   } else {
+//     return "No hay una palabras con mas de 4 letras";
+//   }
+// };
+
+// console.log(buscarPalabras(arrayPalabras));
+
+/*6)
+Utilizando map, crea una función que reciba un array de números 
+y devuelva el mismo array pero con todos los números duplicados.*/
+
+// const arrayNumeros = [2, 4, 6, 8, 10, 12, 14];
+
+// const duplicados = (numeros) => numeros.map((num) => num * 2);
+
+// //Esto es lo mismo que :
+
+// const duplicadosAlt = (numeros) => {
+//   return numeros.map((num) => num * 2);
+// };
+
+// console.log(duplicados(arrayNumeros));
+// /* output:
+// [
+//    4,  8, 12, 16,
+//   20, 24, 28
+// ]
+// */
+
+// console.log(arrayNumeros);
+
+/*7)
+Usando el mismo array de números y map , crea una función que 
+reciba un array de números, un número y una función que realice
+una operación matemática con el número dado y la función pasada 
+como parámetro para cada número del array dado , y devuelva el
+nuevo array con los resultados ( podes utilizar las funciones 
+de suma, resta, multiplicación y división de anteriores 
+prácticas.*/
+
+// const arrayNumeros = [2, 4, 6, 8, 10, 12, 14];
+
+// const sumar = (a, b) => a + b;
+// const restar = (a, b) => a - b;
+// const multiplicar = (a, b) => a * b;
+// const dividir = (a, b) => a / b;
+
+// const realizarOperacion = (numeros, num, operacion) =>
+//   numeros.map((numero) => operacion(numero, num));
+
+// console.log(
+//   realizarOperacion(arrayNumeros, 10, sumar)
+// ); /*output: [12, 14, 16, 18,20, 22, 24]*/
+
+// console.log(
+//   realizarOperacion(arrayNumeros, 10, multiplicar)
+// ); /*output: [20,  40,  60, 80, 100, 120, 140]*/
+
+//Le estamos pasando una función como callback y en base a lo que
+//le pasemos será el output que tengamos
+
+/*8)
+A partir de este ejercicio vamos a trabajar con objetos. Les 
+damos la clase y también 5 películas ya creadas (vamos a usar
+este array para los siguientes ejercicios también).
+
+class Pelicula {
+  constructor(titulo, duracion, año) {
+    this.titulo = titulo;
+    this.duracion = duracion;
+    this.año = año;
+  }
+}
+
+const pokemon = new Pelicula("Pokemon:La Pelicula", 75, 1998);
+const avengers = new Pelicula("Avengers: Endgame", 181, 2019);
+const starWars = new Pelicula(
+  "Star Wars: El despertar de la fuerza",
+  135,
+  2015
+);
+const batman = new Pelicula("Batman vs Superman", 151, 2016);
+const wonderWoman = new Pelicula("La Mujer Maravilla", 141, 2017);
+
+
+Como primera medida, creá un array de películas vacío y mediante 
+métodos de array agrega todas las películas al array vacío e 
+imprimí por consola el array.*/
+
+// class Pelicula {
+//   constructor(titulo, duracion, año) {
+//     this.titulo = titulo;
+//     this.duracion = duracion;
+//     this.año = año;
+//   }
+// }
+
+// const pokemon = new Pelicula("Pokemon:La Pelicula", 75, 1998);
+// const avengers = new Pelicula("Avengers: Endgame", 181, 2019);
+// const starWars = new Pelicula(
+//   "Star Wars: El despertar de la fuerza",
+//   135,
+//   2015
+// );
+// const batman = new Pelicula("Batman vs Superman", 151, 2016);
+// const wonderWoman = new Pelicula("La Mujer Maravilla", 141, 2017);
+
+// const arrayPeliculas = [];
+
+// arrayPeliculas.push(pokemon, avengers, starWars, batman, wonderWoman);
+
+// console.log(arrayPeliculas);
+
+/*9)
+Creá una función que ordene el array por año de salida, desde la
+mas larga hasta la mas corta e imprimí el array por consola .*/
+
+// class Pelicula {
+//   constructor(titulo, duracion, año) {
+//     this.titulo = titulo;
+//     this.duracion = duracion;
+//     this.año = año;
+//   }
+// }
+
+// const pokemon = new Pelicula("Pokemon:La Pelicula", 75, 1998);
+// const avengers = new Pelicula("Avengers: Endgame", 181, 2019);
+// const starWars = new Pelicula(
+//   "Star Wars: El despertar de la fuerza",
+//   135,
+//   2015
+// );
+// const batman = new Pelicula("Batman vs Superman", 151, 2016);
+// const wonderWoman = new Pelicula("La Mujer Maravilla", 141, 2017);
+
+// const arrayPeliculas = [];
+
+// arrayPeliculas.push(pokemon, avengers, starWars, batman, wonderWoman);
+
+// const ordenarPorAño = () => {
+//   arrayPeliculas.sort(function (peli1, peli2) {
+//     if (peli1.año > peli2.año) {
+//       return 1;
+//     }
+//     if (peli1.año < peli2.año) {
+//       return -1;
+//     }
+//     // Si el año de la peli1 es igual al de peli 2.
+//     return 0;
+//   });
+//   console.log(arrayPeliculas);
+// };
+
+// ordenarPorAño();
+
+/*10)
+Crea una función que ordene el array por duración, desde la que
+mas haya durado hasta la que menos. imprimí por consola el 
+array.*/
+
+// class Pelicula {
+//   constructor(titulo, duracion, año) {
+//     this.titulo = titulo;
+//     this.duracion = duracion;
+//     this.año = año;
+//   }
+// }
+
+// const pokemon = new Pelicula("Pokemon:La Pelicula", 75, 1998);
+// const avengers = new Pelicula("Avengers: Endgame", 181, 2019);
+// const starWars = new Pelicula(
+//   "Star Wars: El despertar de la fuerza",
+//   135,
+//   2015
+// );
+// const batman = new Pelicula("Batman vs Superman", 151, 2016);
+// const wonderWoman = new Pelicula("La Mujer Maravilla", 141, 2017);
+
+// const arrayPeliculas = [];
+
+// arrayPeliculas.push(pokemon, avengers, starWars, batman, wonderWoman);
+
+// console.log(arrayPeliculas);
+
+// const ordenarPorDuracion = () => {
+//   arrayPeliculas.sort(function (peli1, peli2) {
+//     if (peli1.duracion > peli2.duracion) {
+//       return -1;
+//     }
+//     if (peli1.duracion < peli2.duracion) {
+//       return 1;
+//     }
+//     // a must be equal to b
+//     return 0;
+//   });
+//   console.log(arrayPeliculas);
+// };
+
+// ordenarPorDuracion();
+
+/*11)
+Crea una función que reciba un array de películas como parámetro
+y devuelva un nuevo array con las películas que duren mas de
+150 minutos.*/
+
+// class Pelicula {
+//   constructor(titulo, duracion, año) {
+//     this.titulo = titulo;
+//     this.duracion = duracion;
+//     this.año = año;
+//   }
+// }
+
+// const pokemon = new Pelicula("Pokemon:La Pelicula", 75, 1998);
+// const avengers = new Pelicula("Avengers: Endgame", 181, 2019);
+// const starWars = new Pelicula(
+//   "Star Wars: El despertar de la fuerza",
+//   135,
+//   2015
+// );
+// const batman = new Pelicula("Batman vs Superman", 151, 2016);
+// const wonderWoman = new Pelicula("La Mujer Maravilla", 141, 2017);
+
+// const arrayPeliculas = [];
+
+// arrayPeliculas.push(pokemon, avengers, starWars, batman, wonderWoman);
+
+// console.log(arrayPeliculas);
+
+// const pelisLargas = (peliculas) =>
+//   peliculas.filter((peli) => peli.duracion > 150);
+
+// // Esto es lo mismo que
+// const pelisLargasAlt = (peliculas) => {
+//   return peliculas.filter((peli) => peli.duracion > 150);
+// };
+// console.log(pelisLargas(arrayPeliculas));
+
+/*12)
+
+Crea una función que reciba un array de películas como parámetro 
+y devuelva un nuevo array con las películas que duren menos de
+150 minutos y sean de un año menor o igual que el año 2016 .*/
+
+// class Pelicula {
+//   constructor(titulo, duracion, año) {
+//     this.titulo = titulo;
+//     this.duracion = duracion;
+//     this.año = año;
+//   }
+// }
+
+// const pokemon = new Pelicula("Pokemon:La Pelicula", 75, 1998);
+// const avengers = new Pelicula("Avengers: Endgame", 181, 2019);
+// const starWars = new Pelicula(
+//   "Star Wars: El despertar de la fuerza",
+//   135,
+//   2015
+// );
+// const batman = new Pelicula("Batman vs Superman", 151, 2016);
+// const wonderWoman = new Pelicula("La Mujer Maravilla", 141, 2017);
+
+// const arrayPeliculas = [];
+
+// arrayPeliculas.push(pokemon, avengers, starWars, batman, wonderWoman);
+
+// console.log(arrayPeliculas);
+
+/*13)
+
+Creá una función que reciba un array de películas y retorne la 
+sumatoria de los minutos de duración de todas las películas del
+mismo. Imprimí luego el siguiente mensaje en consola:
+
+“La cantidad total de minutos de las películas es de {total} 
+minutos”*/
+
+// class Pelicula {
+//   constructor(titulo, duracion, año) {
+//     this.titulo = titulo;
+//     this.duracion = duracion;
+//     this.año = año;
+//   }
+// }
+
+// const pokemon = new Pelicula("Pokemon:La Pelicula", 75, 1998);
+// const avengers = new Pelicula("Avengers: Endgame", 181, 2019);
+// const starWars = new Pelicula(
+//   "Star Wars: El despertar de la fuerza",
+//   135,
+//   2015
+// );
+// const batman = new Pelicula("Batman vs Superman", 151, 2016);
+// const wonderWoman = new Pelicula("La Mujer Maravilla", 141, 2017);
+
+// const arrayPeliculas = [];
+
+// arrayPeliculas.push(pokemon, avengers, starWars, batman, wonderWoman);
+
+// console.log(arrayPeliculas);
+
+// const totalDeMinutos = (peliculas) =>
+//   peliculas.reduce((acc, curr) => acc + curr.duracion, 0);
+
+// // Es lo mismo que poner:
+
+// const totalDeMinutosAlt = (peliculas) => {
+//   let duracionTotal = peliculas.reduce((acc, curr) => acc + curr.duracion, 0);
+//   return duracionTotal;
+// };
+
+// console.log(
+//   `La cantidad total de minutos de las peliculas es de ${totalDeMinutos(
+//     arrayPeliculas
+//   )} minutos.`
+// );
+
+/***************************************************************/
+
+
+
+
